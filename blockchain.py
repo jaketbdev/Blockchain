96e3fe301164396826ff0e755fa99db7dba9bb6f
jaketbdev <72199858+jaketbdev@users.noreply.github.com>
2021-03-16 11:27:13 +0000

    Added flask and created endpoints for the blockchain

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,8 @@ import json
 from time import time
 from uuid import uuid
 
+from flask import Flask
+
 
 class Blockchain(object):
     def __init__(self):
@@ -52,3 +54,33 @@ class Blockchain(object):
     def hash(block):
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+
+
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new block"
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+
+@app.route("/chain", methods=["GET"])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
