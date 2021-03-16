84c9fa9572c9d56742733ce5c37876a5dc3c4f62
jaketbdev <72199858+jaketbdev@users.noreply.github.com>
2021-03-16 11:20:32 +0000

    Added proof of work algoritihm

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid
 
 
 class Blockchain(object):
@@ -29,6 +30,19 @@ class Blockchain(object):
         })
 
         return self.last_block['index'] + 1
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
 
     @property
     def last_block(self):
